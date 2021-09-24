acf11e8a2af0d0695a9a9fc7a6c151d5615a030f
wonderg <gleb.stepanov@gmail.com>
2021-09-24 08:56:46 +0300

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,38 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	newS := ""
+	fmt.Println(len(s))
+	for i := 0; i <= len(s)-1; i++ {
+		fmt.Println(i)
+		//fmt.Println(reflect.TypeOf(rune(s[i])))
+		if unicode.IsDigit(rune(s[i])) {
+			return "", ErrInvalidString
+			//fmt.Println(string(s[i]))
+		} else if i < len(s)-1 && unicode.IsDigit(rune(s[i+1])) {
+			byteToInt, _ := strconv.Atoi(string(s[i+1]))
+			multipliedChar := strings.Repeat(string(s[i]), byteToInt)
+			newS += multipliedChar
+			i++
+		} else {
+			newS += string(s[i])
+		}
+
+	}
+	return newS, nil
 }
+func main() {
+	fmt.Println(Unpack("a4bc2d5e"))
+}
+
+// * "a4bc2d5e" => "aaaabccddddde"
+//https://goplay.tools/snippet/2-SwR6jqjqw
--- notes.md
+++ notes.md
@@ -98,3 +98,21 @@ go mod vendor - to get src code for deps
 # Questions 
 
 - When shift operators could be used? Provide some real life use cases https://stackoverflow.com/questions/5801008/go-and-operators/5801065
+
+
+# Strings 
+[src](https://www.bogotobogo.com/GoLang/GoLang_byte_and_rune.php)
+byte and rune
+Golang has integer types called byte and rune that are aliases for uint8 and int32 data types, respectively.
+
+In Go, the byte and rune data types are used to distinguish characters from integer values.
+
+In Go, there is no char data type. It uses byte and rune to represent character values.
+
+The byte data type represents ASCII characters while the rune data type represents a more broader set of Unicode characters that are encoded in UTF-8 format.
+
+
+
+```
+fmt.Println(reflect.TypeOf('0'))
+```
