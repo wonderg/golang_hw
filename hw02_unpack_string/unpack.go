2322429ff8ffc2751bf02807a94ca94339c939ce
wonderg <gleb.stepanov@gmail.com>
2021-09-24 09:47:06 +0300

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -14,7 +14,6 @@ func Unpack(s string) (string, error) {
 	newS := ""
 	fmt.Println(len(s))
 	for i := 0; i <= len(s)-1; i++ {
-		fmt.Println(i)
 		if unicode.IsDigit(rune(s[i])) {
 			return "", ErrInvalidString
 		} else if i < len(s)-1 && unicode.IsDigit(rune(s[i+1])) {
@@ -28,6 +27,5 @@ func Unpack(s string) (string, error) {
 	}
 	return newS, nil
 }
-
 
 // * "a4bc2d5e" => "aaaabccddddde"
