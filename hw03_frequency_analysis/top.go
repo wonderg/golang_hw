afad90d833c4a4b3d13afab52d0c7ce6edc38716
Gleb Stepanov <gleb.stepanov@ringcentral.com>
2021-09-29 22:57:19 -0600

    Completed HW03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,29 @@
-package hw03frequencyanalysis
+package main
 
-func Top10(_ string) []string {
-	// Place your code here.
+//package hw03frequencyanalysis
+import (
+	"fmt"
+	"strings"
+)
+
+
+
+func Top10(s string) []string {
+	countedList := []struct{
+		word string
+		counter int
+	}
+	list := strings.Fields(s)
+	for q := 0; q < len(list)-1; q++ {
+		if list[q] == list[q+1] {
+			fmt.Println("s:", list[q])
+		}
+	}
 	return nil
+}
+
+func main() {
+	s := "cat and dog, one dog,two cats and one man"
+	Top10(s)
+
 }
