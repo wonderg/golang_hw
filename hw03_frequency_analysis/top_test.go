53ccdd0c9de7d140a9237f0c39b8e190d0d5299f
Gleb Stepanov <gleb.stepanov@ringcentral.com>
2021-12-16 23:32:45 -0700

    //Done + 1 unit test added

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -43,7 +43,7 @@ var text = `Как видите, он  спускается  по  лестни�
 	посидеть у огня и послушать какую-нибудь интересную сказку.
 		В этот вечер...`
 
-var shortText = `cat and dog, one dog,two cats and one man`
+var shortText = `cat and dog, one dog,two cats And one man`
 
 func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
@@ -85,14 +85,14 @@ func TestTop10(t *testing.T) {
 	t.Run("positive test", func(t *testing.T) {
 		if taskWithAsteriskIsCompleted {
 			expected := []string{
-				"and",     // 2
 				"one",     // 2
+				"And",     // 2
+				"and",     // 1
 				"cat",     // 1
 				"cats",    // 1
 				"dog,",    // 1
 				"dog,two", // 1
 				"man",     // 1
-
 			}
 			require.Equal(t, expected, Top10(shortText))
 		} else {
